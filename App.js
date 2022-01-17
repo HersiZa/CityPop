a903c139a1680539a4a13aff1bce443fb70a9a4f
Zakaria Hersi <zakariahersi@outlook.com>
2022-01-17 18:14:08 +0100

    Update App.js
    Fixat en knapp och lärt mig lite syntaxer för att skapa och dekorera en android sida. Se till att förstå API "Geonames" och försök implementera

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ToastAndroid, TouchableOpacity, Button, SafeAreaView, Alert } from 'react-native';
 
 export default function App() {
+  const showToast = () => {
+   console.log ("Toast clicked...")
+   ToastAndroid.show("You clocked this toast",
+   ToastAndroid.SHORT,
+   ToastAndroid.BOTTOM
+   )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Lyckades konfigurera den</Text>
+      <Text style={styles.text}>CityPop</Text>
+      <Text style={styles.text2}>Fixa Api till geonames</Text>
+
+    <TouchableOpacity style = {styles.button} onPress={() => showToast()}>
+      <Text style={{color:"#fff"}}>Press me!</Text>
+    </TouchableOpacity>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +30,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#304ffe",
+    fontSize: 60,
+    padding: 20
+  },
+
+  text2: {
+    color: "#304ffe",
+    fontSize: 40,
+    padding: 25
+  },
+  
+
+  button: {
+    borderRadius: 10,
+    backgroundColor: "#29b6f6",
+    width: 150,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center"
+  }  
 });
